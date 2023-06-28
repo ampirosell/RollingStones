03a2c0760741acda61eab84afbc45cfd4ecfd33e
amparo <amc.vonwu@gmail.com>
2023-06-28 13:38:52 -0300

    no se por qué se modificó solo un template_c y quiero que esté actualizado, no hay otro cambio

--- templates_c/7ff232ed04441b3549c7437b6844b6b9d0f63093_0.file.sobre.tpl.php
+++ templates_c/7ff232ed04441b3549c7437b6844b6b9d0f63093_0.file.sobre.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-05-27 21:16:14
+/* Smarty version 4.3.1, created on 2023-06-28 04:07:58
   from 'C:\xampp\htdocs\RollingStones\templates\sobre.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_647256fe58c506_11475442',
+  'unifunc' => 'content_649b95fee85328_85034186',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7ff232ed04441b3549c7437b6844b6b9d0f63093' => 
     array (
       0 => 'C:\\xampp\\htdocs\\RollingStones\\templates\\sobre.tpl',
-      1 => 1685214961,
+      1 => 1687918075,
       2 => 'file',
     ),
   ),
@@ -22,12 +22,27 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_647256fe58c506_11475442 (Smarty_Internal_Template $_smarty_tpl) {
+function content_649b95fee85328_85034186 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-<div class="content">
-    <h3>Trabajo de vanina y amparo</h3>
-    <p>Lorem Ipsus...</p>
+<div class="content"  >
+    <h3>Trabajo de Amparo Rosell y Vanina Pozzobon</h3>
+    <p>¡Bienvenidos a nuestra página "Sobre nosotras"!
+    <br>
+    <br>
+    Somos <b>Amparo Rosell y Vanina Pozzobon</b>, dos alumnas de la Tecnicatura en Desarrollo de Aplicaciones Informáticas. Nos complace compartir un poco sobre quiénes somos y qué nos impulsa en el mundo de la tecnología.
+    <br>
+    Desde jóvenes, hemos sentido una profunda curiosidad por la informática y el desarrollo de aplicaciones. Nos cautiva la forma en que la tecnología transforma la sociedad y la manera en que interactuamos con el mundo. Nuestra dedicación y compromiso nos llevaron a elegir esta carrera, donde podemos dar rienda suelta a nuestra creatividad y encontrar soluciones innovadoras a los desafíos del mundo digital.
+    <br>
+    Durante nuestro recorrido en la Tecnicatura en Desarrollo de Aplicaciones Informáticas, hemos adquirido conocimientos sólidos en programación, diseño de interfaces, bases de datos y desarrollo de aplicaciones web. Estamos constantemente aprendiendo nuevas tecnologías para estar al tanto de las últimas tendencias y ofrecer servicios de calidad.
+    <br>
+    Nos enorgullece poder aplicar nuestras habilidades y conocimientos para crear soluciones efectivas y eficientes.
+    <br>
+    En nuestra trayectoria, hemos trabajado en equipo en diversos proyectos, lo que nos ha brindado una perspectiva más amplia y nos ha enseñado la importancia de la colaboración y la comunicación efectiva. Valoramos la diversidad de ideas y creemos en la fuerza de un equipo multidisciplinario para lograr resultados excepcionales.
+    <br>
+    Nos motiva la idea de seguir aprendiendo y creciendo en este campo en constante evolución. 
+    <br>
+    ¡Gracias por visitar nuestra página!</p>
 </div>
 <?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
